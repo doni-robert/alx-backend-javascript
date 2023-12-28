e134ea19149ea229d5b6e838492e157da5037b12
doni-robert <irobertndungu@gmail.com>
2023-12-28 10:17:47 +0300

    correct lint

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -5,10 +5,10 @@ const http = require('http');
 
 // Create a local server to receive data from
 const app = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello Holberton School!')
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
 });
 
-app.listen(1245)
+app.listen(1245);
 
 module.exports = app;
--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,13 +1,12 @@
 /**
- * 
+ *
  */
 const express = require('express');
 
 app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello Holberton School!');
-
-})
+  res.send('Hello Holberton School!');
+});
 
 app.listen(1245);
