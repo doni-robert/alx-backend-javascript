2a316549fe03dbbd738895d4732ac259483a9cde
doni-robert <irobertndungu@gmail.com>
2024-03-08 12:49:03 +0300

    write tests

---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,31 @@
+
+const request = require("request");
+const {describe, it} = require("mocha");
+const expect = require("chai").expect;
+
+describe("Index page", function() {
+    const options = {
+	url: "http://localhost:7865/",
+	method: "GET"
+    }
+    it("checking correct status code", function(done) {
+	request(options, function(err, res, body) {
+	    expect(res.statusCode).to.equal(200);
+	    done();
+	});
+
+    });
+    it("checking correct content", function(done) {
+	request(options, function(err, res, body) {
+	    expect(body).to.contain("Welcome to the payment system");
+	    done();
+	});
+    
+    });
+    it("checking correct content length", function(done) {
+	request(options, function(err, res, body) {
+	    expect(res.headers['content-length']).to.equal('29');
+	    done();
+	});
+    });
+});
