4d9c3fcfb4698b05b35533b8eb7458ebc2fc7c96
doni-robert <irobertndungu@gmail.com>
2024-03-23 08:39:26 +0300

    refractor: rewrite stdin.on

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,11 +2,9 @@
  * Program that takes an input and displays it
  */
 console.log('Welcome to Holberton School, what is your name?');
-process.stdin.on('readable', () => {
-  const input = process.stdin.read();
-  if (input !== null) {
-    process.stdout.write(`Your name is: ${input}`);
-  }
+
+process.stdin.on('data', (chunk) => {
+  process.stdout.write(`Your name is: ${chunk}`);
 });
 
 process.stdin.on('end', () => {
