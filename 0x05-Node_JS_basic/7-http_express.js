d69c760e297276b88c23b679985180fe740da7e9
doni-robert <irobertndungu@gmail.com>
2024-03-24 12:47:25 +0300

    fix: correct error message

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,18 +1,23 @@
+// Importing required modules
 const express = require('express');
 const students = require('./3-read_file_async');
 
+// Setting up server configurations
 const app = express();
 const hostname = '127.0.0.1';
 const port = 1245;
 
+// Handling GET requests to the root route '/'
 app.get('/', (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.send('Hello Holberton School!');
 });
 
+// Handling GET requests to the '/students' route
 app.get('/students', async (req, res) => {
   try {
+    // Attempting to load student data asynchronously
     const data = await students(process.argv[2]);
     const response = `Number of students: ${data.students.length}\n`
       + `Number of students in CS: ${data.csStudents.length}. List: ${data.csStudents.join(', ')}\n`
@@ -22,12 +27,14 @@ app.get('/students', async (req, res) => {
     res.setHeader('Content-Type', 'text/plain');
     res.send(`This is the list of our students\n${response}`);
   } catch (err) {
+    // Handling errors if data loading fails
     res.statusCode = 500;
     res.setHeader('Content-Type', 'text/plain');
-    res.send(err.message);
+    res.send(`This is the list of our students\n${err.message}`);
   }
 });
 
+// Starting the server and listening for incoming connections
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}`);
 });
